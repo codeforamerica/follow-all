12fccbe8ea2d007ae2acc3e11f8fbba50e44347b
Erik Michaels-Ober <sferik@gmail.com>
2011-01-05 19:42:11 -0800

    Add followers logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TwitterApp::Application.routes.draw do
-  get 'sessions/callback', :to => 'sessions#callback', :as => 'callback'
+  get 'sessions/callback', :to => 'sessions#callback', :as => :callback
   resources :sessions
+  resources :follows
   root :to => 'sessions#new'
 end
