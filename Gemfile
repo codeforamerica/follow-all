22a467b6646c65a49a0eba90ede2614735901578
Erik Michaels-Ober <sferik@gmail.com>
2011-01-14 14:35:45 -0800

    Hack to make tests work
    https://github.com/rspec/rspec-rails/issues/301

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.3'
 gem 'oauth', '~> 0.4.4'
 gem 'twitter', '~> 1.1.0'
+gem 'sqlite3-ruby', :require => 'sqlite3'
 
 group :test, :development do
   gem 'rspec'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'action_controller/railtie'
+# require 'action_controller/railtie'
+require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
