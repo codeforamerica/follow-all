321e393e1a7fc6fc93b91f2c48344b776fe9fb4c
Erik Michaels-Ober <sferik@gmail.com>
2012-01-10 14:43:15 -0800

    Update rails dependency to version 3.2.0.rc2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-FollowAll::Application.config.session_store :cookie_store, :key => '_sign-in-with-twitter_session'
+FollowAll::Application.config.session_store :cookie_store, :key => '_follow-all_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
