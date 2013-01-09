26a3d88ff56fcf9c7e02bcf56d3812f4c0ef29d1
Ryan Closner <ryanc@codeforamerica.org>
2013-01-08 16:57:30 -0800

    Explicity specify assets to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ FollowAll::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.js polyfills.js application.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
