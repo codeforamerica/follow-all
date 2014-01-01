27470b45a9b3a2f811e04c97348675eaaaface6a
Erik Michaels-Ober <sferik@gmail.com>
2014-01-01 04:58:40 +0100

    Don't make API request when User is initialized

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,14 @@
 class User
-  attr_reader :client, :id
-
   def initialize(client)
     @client = client
-    user = client.user
-    @id = user.id
   end
 
   def follow_list(user, list)
-    users_to_follow = client.list_members(user, list, :skip_status => true, :include_entities => false).to_a
+    users_to_follow = @client.list_members(user, list, :skip_status => true, :include_entities => false).to_a
     num_attempts = 0
     begin
       num_attempts += 1
-      client.follow(users_to_follow)
+      @client.follow(users_to_follow)
     rescue Twitter::Error::TooManyRequests => error
       if num_attempts <= 3
         sleep error.rate_limit.reset_in
