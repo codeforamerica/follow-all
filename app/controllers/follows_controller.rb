32c859f9527967b99b02d186c454b5187cc019c6
Erik Michaels-Ober <sferik@gmail.com>
2014-08-15 15:46:19 +0200

    Refactor FollowsController#create

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,22 +1,26 @@
 class FollowsController < ApplicationController
-  def create # rubocop:disable MethodLength
+  def create
     if session['access_token'] && session['access_token_secret']
-      begin
-        @user = User.new(client)
-        @new_friends = @user.follow_list(*params[:list].split('/'))
-        if @new_friends.size.zero?
-          flash.now[:notice] = 'You are already following everyone on this list.'
-        elsif @new_friends.size == 1
-          flash.now[:notice] = 'You are now following 1 new person.'
-        else
-          flash.now[:notice] = "You are now following #{@new_friends.size} new people."
-        end
-      rescue Twitter::Error::BadRequest
-        flash.now[:notice] = 'You have been rate-limited by Twitter. Please try again in an hour.'
-      end
+      set_flash
       render 'sessions/show'
     else
       redirect_to failure_path
     end
+  end
+
+private
+
+  def set_flash
+    @user = User.new(client)
+    @new_friends = @user.follow_list(*params[:list].split('/'))
+    if @new_friends.size.zero?
+      flash.now[:notice] = 'You are already following everyone on this list.'
+    elsif @new_friends.size == 1
+      flash.now[:notice] = 'You are now following 1 new person.'
+    else
+      flash.now[:notice] = "You are now following #{@new_friends.size} new people."
+    end
+  rescue Twitter::Error::BadRequest
+    flash.now[:notice] = 'You have been rate-limited by Twitter. Please try again in an hour.'
   end
 end
